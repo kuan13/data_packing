3d247522a03b669f634f8a33c2caf78cf693a685
kuan13 <yukuan13@smail.nchu.edu.tw>
2022-10-30 21:46:39 +0800

    add charts

---
+++ src/pages/kla/components/charts.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: 'My chart'
+  },
+  series: [
+    {
+      data: [3, 2, 1, 4, 5, 6]
+    }
+  ]
+};
+
+
+const Charts = () => {
+ 
+    return (
+      <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+    );
+
+};
+
+export default Charts;
--- src/pages/kla/components/tabs.js
+++ src/pages/kla/components/tabs.js
@@ -2,6 +2,7 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 import BasicExample from './table';
+import Charts from './charts'
 
 const UncontrolledExample = () => {
   return (
@@ -12,11 +13,15 @@ const UncontrolledExample = () => {
     >
       <Tab eventKey="tab1" title="主頁">
         <BasicExample />
+        <Charts />
       </Tab>
       <Tab eventKey="tab2" title="wafermap">
         <BasicExample />
       </Tab>
-      <Tab eventKey="tab3" title="others" disabled>
+      <Tab eventKey="tab4" title="test">
+        <Charts />
+      </Tab>      
+      <Tab eventKey="tab5" title="others" disabled>
         <BasicExample />
       </Tab>
     </Tabs>
